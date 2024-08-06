215a1a4fad83369f14d85581368e99c7ae9ffc8e
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-08-06 20:03:56 +0530

    Added gender screen

--- app/src/main/java/com/vinayakgupta3112/calorytracker/MainActivity.kt
+++ app/src/main/java/com/vinayakgupta3112/calorytracker/MainActivity.kt
@@ -9,8 +9,11 @@ import androidx.navigation.compose.rememberNavController
 import com.vinayakgupta3112.calorytracker.navigation.navigate
 import com.vinayakgupta3112.calorytracker.ui.theme.CaloryTrackerTheme
 import com.vinayakgupta3112.core.navigation.Route
+import com.vinayakgupta3112.onboarding_presentation.gender.GenderScreen
 import com.vinayakgupta3112.onboarding_presentation.welcome.WelcomeScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +28,7 @@ class MainActivity : ComponentActivity() {
 
                     }
                     composable(Route.GENDER) {
-
+                        GenderScreen(onNavigate = navController::navigate)
                     }
                     composable(Route.HEIGHT) {
 
