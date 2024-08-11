23c4be9f6d4e4e7f5e414d533b1ba55cfece77de
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-08-12 01:52:38 +0530

    started unit testing

---
+++ tracker/tracker_domain/src/test/java/com/vinayakgupta3112/tracker_domain/use_case/CalculateMealNutrientsTest.kt
@@ -0,0 +1,94 @@
+package com.vinayakgupta3112.tracker_domain.use_case
+
+import com.google.common.truth.Truth.assertThat
+import com.vinayakgupta3112.core.domain.model.ActivityLevel
+import com.vinayakgupta3112.core.domain.model.Gender
+import com.vinayakgupta3112.core.domain.model.GoalType
+import com.vinayakgupta3112.core.domain.model.UserInfo
+import com.vinayakgupta3112.core.domain.preferences.Preferences
+import com.vinayakgupta3112.tracker_domain.model.MealType
+import com.vinayakgupta3112.tracker_domain.model.TrackedFood
+import com.vinayakgupta3112.tracker_domain.repository.TrackerRepository
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Before
+import org.junit.Test
+import java.time.LocalDate
+import kotlin.random.Random
+
+class CalculateMealNutrientsTest {
+    private lateinit var calculateMealNutrients: CalculateMealNutrients
+
+    @Before
+    fun setUp() {
+        val preferences = mockk<Preferences>(relaxed = true)
+        every { preferences.loadUserInfo() } returns UserInfo(
+            gender = Gender.Male,
+            age = 20,
+            weight = 20f,
+            height = 180,
+            activityLevel = ActivityLevel.Medium,
+            goalType = GoalType.KeepWeight,
+            carbRatio = 0.4f,
+            proteinRatio = 0.3f,
+            fatRatio = 0.3f
+        )
+        calculateMealNutrients = CalculateMealNutrients(preferences)
+    }
+
+    @Test
+    fun `Calories for breakfast properly calculated`() {
+        val trackedFoods = (1..30).map {
+            TrackedFood(
+                name = "name",
+                carbs = Random.nextInt(100),
+                protein = Random.nextInt(100),
+                fat = Random.nextInt(100),
+                mealType = MealType.fromString(
+                    listOf("breakfast", "lunch", "dinner", "snack").random()
+                ),
+                imageUrl = null,
+                amount = 100,
+                date = LocalDate.now(),
+                calories = Random.nextInt(2000)
+            )
+        }
+        val result = calculateMealNutrients(trackedFoods)
+        val breakfastCalories = result.mealNutrients.values
+            .filter { it.mealType is MealType.Breakfast }
+            .sumOf { it.calories }
+        val expectedCalories = trackedFoods
+            .filter { it.mealType is MealType.Breakfast }
+            .sumOf { it.calories }
+
+        assertThat(breakfastCalories).isEqualTo(expectedCalories)
+    }
+
+    @Test
+    fun `Carbs for Dinner properly calculated`() {
+        val trackedFoods = (1..30).map {
+            TrackedFood(
+                name = "name",
+                carbs = Random.nextInt(100),
+                protein = Random.nextInt(100),
+                fat = Random.nextInt(100),
+                mealType = MealType.fromString(
+                    listOf("breakfast", "lunch", "dinner", "snack").random()
+                ),
+                imageUrl = null,
+                amount = 100,
+                date = LocalDate.now(),
+                calories = Random.nextInt(2000)
+            )
+        }
+        val result = calculateMealNutrients(trackedFoods)
+        val dinnerCarbs = result.mealNutrients.values
+            .filter { it.mealType is MealType.Dinner }
+            .sumOf { it.carbs }
+        val expectedCarbs = trackedFoods
+            .filter { it.mealType is MealType.Dinner }
+            .sumOf { it.carbs }
+
+        assertThat(dinnerCarbs).isEqualTo(expectedCarbs)
+    }
+}
